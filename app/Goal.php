4e55498437dfc8d3993ff9d18ad12931ecfe6e9c
igatake <t.igai.716@gmail.com>
2020-05-02 07:37:27 +0900

    custumized goal and task model

--- app/Goal.php
+++ app/Goal.php
@@ -3,8 +3,18 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
 
 class Goal extends Model
 {
-    //
+
+    public function tasks() {
+        return $this->hasMany('App\task');
+    }
+
+
+    public function getFormattedDueDateAttribute() {
+        return Carbon::createFromFormat('Y-m-d', $this->attributes['due_date'])
+            ->format('Y/m/d');
+    }
 }
--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -3,17 +3,27 @@
 namespace App\Http\Controllers;
 
 use App\Goal;
+use App\Task;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
-{
+{   
+
     public function index(int $id)
     {
+        // 全てのGoalを取得する
         $goals = Goal::all();
+
+        // 選ばれたGoalを取得する
+        $current_goal = Goal::find($id);
+
+        // 選ばれたGoalに紐づくTaskを取得する
+        $tasks = $current_goal->tasks()->get();
 
         return view('tasks/index', [
             'goals' => $goals,
-            'current_goal_id' => $id,
+            'current_goal_id' => $current_goal->id,
+            'tasks' => $tasks,
         ]);
     }
 }
